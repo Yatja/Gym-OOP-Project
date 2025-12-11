ca98da2bf15b979112c61c6c03e5e0241134ca61
Ali Yerzhanuly <252739@astanait.edu.kz>
2025-12-11 18:47:32 +0500

    Add files via upload
    Gym app  desription

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] Git){
+        System.out.println("Welcome to GYM");
+        System.out.println("My system is digital assist tool");
+        System.out.println("It helps to introduce with Gym");
+        System.out.println("Also it can share peoples review");
+        System.out.print("And it is communication tool between seller and people");
+    }
+
+
+}
