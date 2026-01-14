4d4206bf9a785ba3c913af82001828249e38449d
Ali Yerzhanuly <252739@astanait.edu.kz>
2026-01-14 09:07:55 +0500

    Update Member.java

--- Member.java
+++ Member.java
@@ -1,31 +1,35 @@
 package Gym_system;
 
-public class Member {
-    private int id;
-    private String name;
+public class Member extends Person {
+
     private int age;
     private boolean active;
-    public Member(int id, String name, int age, boolean active) {
-        if (id <= 0) throw new IllegalArgumentException("ID must be positive");
-        if (name == null || name.isBlank()) throw new IllegalArgumentException("Name cannot be empty");
-        if (age < 0) throw new IllegalArgumentException("Age cannot be negative");
 
-        this.id = id;
-        this.name = name;
-        this.age = age;
+    public Member(int id, String name, int age, boolean active) {
+        super(id, name);   // super keyword (FIRST line)
+        setAge(age);       // constructor uses setter
         this.active = active;
     }
+
+    public void setAge(int age) {
+        if (age >= 0) {
+            this.age = age;
+        } else {
+            throw new IllegalArgumentException("Age cannot be negative");
+        }
+    }
+
+    @Override
+    public void showInfo() {
+        System.out.println("Member: " + name + ", age=" + age + ", active=" + active);
+    }
+
     public void deactivate() {
         active = false;
     }
-    public boolean isAdult() {
-        return age >= 18;
-    }
-    @Override
-    public String toString() {
-        return "Member{id=" + id + ", name='" + name + "', age=" + age + ", active=" + active + "}";
-    }
 }
+
+
 
 
 
